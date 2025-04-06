4c3aed121cebea1e4ebdadf0a972e9a40715399e
Leoclimbes <leoclimbes2@gmail.com>
2025-04-06 10:38:43 -0500

    i did some practice with .push and .pop

--- arraypractice.js
+++ arraypractice.js
@@ -1,4 +1,7 @@
 let luna = ["dog", "furry", "black", "funny", "fast", "energetic", "aussie-doodle", "cute"];
+luna.push("the best dog ever");
+let popped = luna.pop();
+console.log(popped);
 console.log(luna);
 luna[luna.length - 5] = "tired";
 console.log(luna);
