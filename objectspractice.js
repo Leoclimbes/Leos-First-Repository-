b357fd9a2951d23d051e0c8cfeb29d59af68bbfb
Leoclimbes <leoclimbes2@gmail.com>
2025-04-15 20:01:54 -0500

    practice

---
+++ objectspractice.js
@@ -0,0 +1,12 @@
+const ben = {
+  name: "ben",
+  "cool ness": "100 out of 10",
+  funniness: "11 out of 10"
+};
+console.log(ben["cool ness"]);
+const leo = {
+  name: "leo",
+  "cool ness": "2000 out of 10",
+  funniness: "100 out of 10"
+};
+console.log(leo.name);
