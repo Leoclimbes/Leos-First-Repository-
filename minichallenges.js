85383ce1dfc339ed9fd9c48d343532352b8f4ce9
Leoclimbes <leoclimbes2@gmail.com>
2025-09-21 11:27:36 -0500

    Challenges
    I completed a few more js challenges

---
+++ minichallenges.js
@@ -0,0 +1,149 @@
+//console.log(Math.floor(Math.random() * 10) + 1)
+
+//let caps = "HELLO"
+//console.log(caps.toLowerCase())
+
+//let lower = "hell0"
+//console.log(lower.toUpperCase())
+
+/*function sayHello() {
+    console.log("Hello")
+}
+sayHello()*/
+
+/*let phrase = "I like luna"
+function printMessage(message) {
+    console.log(message);
+}
+
+printMessage(phrase)*/
+
+
+/*function greeting(person) {
+    console.log(`Hello ${person}`)
+}
+
+greeting("Leo")*/
+
+/*let text = "I like apples"
+function lengthOfString(string) {
+    console.log(string.length)
+}
+lengthOfString(text);*/
+
+/*function firstLetterofString(word) {
+    console.log(word[0])
+}
+firstLetterofString("Coding")*/
+
+/*function lastLetterofstring(word) {
+    console.log(word.slice(-1))
+}
+lastLetterofstring("Computer")*/
+
+/*function square(x) {
+    console.log(x ** 2)
+}
+square(4)*/
+
+//console.log(10 > 5);
+
+/*function isEven(x) {
+    console.log(x % 2 === 0)
+}
+isEven(9)*/
+
+//console.log(14 % 3)
+
+/*function multiply(x,y) {
+    console.log(x * y)
+}
+multiply(3,8)*/
+
+/*function add(x,y) {
+    console.log(`${x} + ${y} = ${x+y}`)
+}
+add(5,5)
+
+function subtract(x,y) {
+    console.log(`${x} - ${y} = ${x-y}`)
+}
+subtract(5,5)
+
+function multiply(x,y) {
+    console.log(`${x} * ${y} = ${x*y}`)
+}
+multiply(5,5)
+
+function divide(x,y) {
+    console.log(`${x} / ${y} = ${x/y}`)
+}
+divide(5,5)*/
+
+/*let favriotes = ["Climbing", "Muay Thai", "Rnning", "Mountain Biking", "Coding"]
+function printFavriotes(){
+    for(let i = 0; i < 5; i++){
+        console.log(`I love ${favriotes[i]}`)
+    }
+}
+printFavriotes()*/
+
+/*function toFarinheight(C) {
+    console.log(`${Math.round(C * 1.8 + 32)} Farinheight `)
+}
+function toCelcius(F){
+    console.log(`${Math.round((F - 32) * 5/9)} Celcius`)
+}
+toFarinheight(3)
+toCelcius(8)*/
+
+/*function countWords(string) {
+    console.log(string.length)
+}
+countWords("Hello i like apple.")*/
+
+/*function doubleNumber(x){
+    console.log(x * 2)
+}
+doubleNumber(5)*/
+
+/*let array = [2, 4, 16]
+function square() {
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i] ** 2)
+}
+}
+square()*/
+
+/*function reverseWord(string) {
+    let reversed = string.split("").reverse().join("");
+    console.log(reversed)
+}
+reverseWord("Apple")*/
+
+/*function isEvenOrOdd(x) {
+if(x % 2 == 0) {
+    console.log(`${x} is even`)
+} else {
+    console.log(`${x} is odd`)
+}
+}
+isEvenOrOdd(4)*/
+
+/*function findLargest(x,y,z) {
+    let largest = Math.max(x,y,z)
+    console.log(`${largest} is the largest number.`)
+}
+findLargest(72,56,89)*/
+
+/*function sumOfAnArray(x) {
+    let total = 0;
+    for(let i = 0; i < x.length; i++){
+        total += x[i]
+    }
+    console.log(total)
+}
+sumOfAnArray([1,2,3,4])*/
+
+
+
